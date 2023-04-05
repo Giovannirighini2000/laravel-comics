663ce49e21214ccc45dfc8bb74624f090f178574
Giovannirighini2000 <righinigiovanni0@gmail.com>
2023-04-05 16:03:42 +0200

    feat added jumbo img

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,21 @@
+<style lang="scss">
+    .container-img-jumbo {
+        height: 450px;
+        overflow: hidden;
+    }
+    
+    .jumbo {
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+        object-position: center top;
+    }
+    </style>
 @extends('layout.app')
 
     @section('content')
-    <div>
-        ciao
+    <div class="container-img-jumbo">
+        <img class="jumbo" src="{{ Vite::asset('resources/img/jumbotron.jpg') }}" alt="">
     </div>
         
     @endsection
